3a3b373bdc3fa524a86c06d2a3da52a4d1822b32
ggueli <gianluca.gue99@gmail.com>
2025-04-15 23:34:10 +0200

    chore(spotify): improve structure

--- src/main/java/com/meaningfulplaylists/infrastructure/spotify/configs/SpotifyConfig.java
+++ src/main/java/com/meaningfulplaylists/infrastructure/spotify/configs/SpotifyConfig.java
@@ -4,7 +4,6 @@ import com.meaningfulplaylists.infrastructure.spotify.SpotifyAccount;
 import com.meaningfulplaylists.infrastructure.spotify.SpotifyApi;
 import com.meaningfulplaylists.infrastructure.retrofit.RetrofitUtils;
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Getter
